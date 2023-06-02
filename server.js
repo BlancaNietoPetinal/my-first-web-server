a7909c78c34c2e8692ef10e87233b5764ccc0bb9
Blanca Nieto <blancanietopetinal@gmail.com>
2023-06-02 19:09:38 +0200

    Changed home path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.send("yasss queen")
+app.get('/', (req, res) => {
+    res.send("Holi queen")
 })
 
 app.listen(3000, ()=> console.log('Listening on port 3000'))
